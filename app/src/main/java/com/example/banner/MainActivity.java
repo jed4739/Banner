0d65043f94737548df91075fd5b2e62fa01536d5
jed4739 <tjsdlr04@gmail.com>
2022-12-13 10:25:23 +0900

    start 2022.12.13 0

--- app/src/main/java/com/example/banner/MainActivity.java
+++ app/src/main/java/com/example/banner/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.banner.databinding.Main;
 
+public class MainActivity extends AppCompatActivity {
+    Main binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding.
     }
 }
