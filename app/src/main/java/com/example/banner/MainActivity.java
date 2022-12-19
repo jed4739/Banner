966f904b28bab823b1ac449d3f6d4bfa3f79d582
jed4739 <tjsdlr04@gmail.com>
2022-12-19 12:48:21 +0900

    완성 2022.12.19 -2

--- app/src/main/java/com/example/banner/MainActivity.java
+++ app/src/main/java/com/example/banner/MainActivity.java
@@ -1,34 +1,32 @@
 package com.example.banner;
 
-import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.example.banner.databinding.Main;
 import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
 
 import me.relex.circleindicator.CircleIndicator3;
 
 public class MainActivity extends FragmentActivity {
 
     private Main binding;
+    private MyAdapter myAdapter;
+
     private int page = 3;
     private int tab_pos;
+
     private ViewPager2 viewPager;
-    private MyAdapter myAdapter;
     private CircleIndicator3 indicator;
-    private final Handler sliderHandler = new Handler();
-    Runnable sliderRunnable;
     private TabLayout tabLayout;
+
+    private final Handler sliderHandler = new Handler();
+    private Runnable sliderRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,34 +44,38 @@ public class MainActivity extends FragmentActivity {
     private void autoSlider() {
         sliderRunnable = () -> binding.viewPager.setCurrentItem(binding.viewPager.getCurrentItem() + 1);
     }
-    // 뷰페이저와 어답터 연결
+    // 뷰페이저와 어답터클래스 연결
     private void setViewPagerAdapter(){
         viewPager = binding.viewPager;
         myAdapter = new MyAdapter(this, page);
         viewPager.setAdapter(myAdapter);
     }
-
     // 뷰페이저와 인디케이터 연결
     private void setIndicator() {
         indicator = binding.indicator;
         indicator.setViewPager(viewPager);
         indicator.createIndicators(page, 0);
     }
-
     // 뷰페이저와 탭레이아웃 연결 및 설정
     private void setTabLayout() {
         tabLayout = binding.tab;
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            /*
+            * 현재 선택중이지 않은 다른 탭을 선택시 실행되는 메소드
+            * 다시 선택한 탭의 위치를 tabBtn 메소드로 넘김.
+            * */
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 tab_pos = tab.getPosition();
                 tabBtn(tab_pos);
             }
-
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
             }
-
+            /*
+            * 현재 선택한 탭을 다시 선택 시 실행되는 메소드
+            * 다시 선택한 탭의 위치를 tabBtn 메소드로 넘김.
+            * */
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
                 tab_pos = tab.getPosition();
@@ -117,7 +119,6 @@ public class MainActivity extends FragmentActivity {
             default:
         }
     }
-
     /*
     * setOrientation -> 가로 스크롤
     * setCurrentItem -> 시작 아이템 위치
@@ -130,7 +131,6 @@ public class MainActivity extends FragmentActivity {
         viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
         viewPager.setCurrentItem(5001);
         viewPager.setOffscreenPageLimit(3);
-
         viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -142,7 +142,6 @@ public class MainActivity extends FragmentActivity {
                     sliderHandler.postDelayed(sliderRunnable, 3000);
                 }
             }
-
             // 스크롤이 무한대일 경우 3 -> 1 로 인디케이터 설정
             @Override
             public void onPageSelected(int position) {
